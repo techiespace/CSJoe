d5081fb8e39894dd7b3635c099dd812bdbcc1005
Shreyas <shreyas.techiespace@gmail.com>
2018-02-13 07:26:36 +0530

    Add GitHub link in overflow menu

--- app/src/main/java/com/techiespace/projects/csjoe/MainActivity.java
+++ app/src/main/java/com/techiespace/projects/csjoe/MainActivity.java
@@ -1,10 +1,16 @@
 package com.techiespace.projects.csjoe;
 
+import android.annotation.SuppressLint;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.view.menu.MenuBuilder;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import butterknife.BindView;
 
@@ -27,5 +33,26 @@ public class MainActivity extends AppCompatActivity {
         fragmentTransaction.commit();
     }
 
+    @SuppressLint("RestrictedApi")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        if(menu instanceof MenuBuilder){
+            MenuBuilder m = (MenuBuilder) menu;
+            m.setOptionalIconsVisible(true);
+        }
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.github:
+                Intent browser = new Intent(Intent.ACTION_VIEW, Uri.parse("https://github.com/techiespace/CSJoe"));
+                startActivity(browser);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
 }
